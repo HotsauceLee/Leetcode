7964593f02ab69694435ed0d0ab4a3e7592281a3
HotsauceLee <boy27910230@hotmail.com>
2016-04-24 15:45:07 -0500

    101(queue)

--- Tree/Easy/101.Symmetric_Tree.py
+++ Tree/Easy/101.Symmetric_Tree.py
@@ -24,3 +24,33 @@ class Solution(object):
         if root1.val != root2.val: return False
         
         return self.helper(root1.left, root2.right) and self.helper(root1.right, root2.left)
+
+
+######## Queue ##############
+# Running time: 56ms
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        q = [root, root]
+        while len(q) > 0:
+            node1 = q.pop()
+            node2 = q.pop()
+            if node1 is None and node2 is None:
+                continue
+            if node1 is None or node2 is None:
+                return False
+            if node1.val != node2.val:
+                return False
+                
+            q.insert(0, node1.left)
+            q.insert(0, node2.right)
+            q.insert(0, node1.right)
+            q.insert(0, node2.left)
+                
+        return True
