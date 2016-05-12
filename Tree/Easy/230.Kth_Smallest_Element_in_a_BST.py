68cbd220324e410f11f31774f0e07fa1c1794828
HotsauceLee <boy27910230@hotmail.com>
2016-05-12 11:27:03 -0500

    Update 230.Kth_Smallest_Element_in_a_BST.py

--- Tree/Easy/230.Kth_Smallest_Element_in_a_BST.py
+++ Tree/Easy/230.Kth_Smallest_Element_in_a_BST.py
@@ -69,3 +69,46 @@ class Solution(object):
                 stack.append(node.right)
             
         return -1
+
+
+######## Resursion ##############
+# BTS, count the # of node on left side and decide which way to go
+# Running time: 
+# Time complexity:
+# Space complexity: 
+class Solution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        c = self.count(root.left)
+        if k == c + 1:
+            return root.val
+        if k <= c:
+            return self.kthSmallest(root.left, k)
+        
+        return self.kthSmallest(root.right, k - 1 - c)
+            
+
+    def count(self, root):
+        if root is None:
+            return 0
+            
+        return 1 + self.count(root.left) + self.count(root.right)
+        
+# Follow up:
+# What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? How would you optimize the kthSmallest routine?
+# A: Modify tree node object to have both left and right node count, and do BTS to find the kth.
+# When inserting/deleting, update the node count of either side when traversing the tree.
+# Time complexity: O(height)
+# Space complexity: O(height)
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.left_count = 0
+#         self.right = None
+#         self.right_count = 0
