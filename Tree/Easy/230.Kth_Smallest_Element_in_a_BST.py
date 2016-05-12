66eac2195ddfb74350f4cc87b7b7e32f65ed8a17
HotsauceLee <boy27910230@hotmail.com>
2016-05-12 10:35:31 -0500

    Update 230.Kth_Smallest_Element_in_a_BST.py

--- Tree/Easy/230.Kth_Smallest_Element_in_a_BST.py
+++ Tree/Easy/230.Kth_Smallest_Element_in_a_BST.py
@@ -38,3 +38,34 @@ class Solution(object):
         self.kth -= 1
         
         self.helper(root.right)
+        
+        
+######## Resursion ##############
+# Running time: 84ms
+# Time complexity: O(n)
+# Space complexity: O(n)
+class Solution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        stack = [root]
+        
+        while stack:
+            last_node = stack[-1]
+            if last_node.left is not None:
+                stack.append(last_node.left)
+                last_node.left = None
+                continue
+            
+            node = stack.pop()
+            if k == 1:
+                return node.val
+            k -= 1
+            
+            if node.right is not None:
+                stack.append(node.right)
+            
+        return -1
