4ba9a223b128f2f19df3f36f525589284e73c8ba
HotsauceLee <boy27910230@hotmail.com>
2016-06-27 08:07:16 -0500

    98

---
+++ Tree/Medium/R__98.Validate_Binary_Search_Tree.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+######## Resursion with min and max ##############
+# Running time: 72ms
+# Time complexity: O(n)
+# Space complexity: O(n)
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        return self.helper(root, -sys.maxint - 1, sys.maxint)
+        
+    def helper(self, root, min, max):
+        if root is None:
+            return True
+            
+        if ((root.left is not None and root.left.val >= root.val) or root.val >= max):
+            return False
+            
+        if ((root.right is not None and root.right.val <= root.val) or root.val <= min):
+            return False
+            
+        return self.helper(root.left, min, root.val) and self.helper(root.right, root.val, max)
+        
