bd31af0618f7a408faca3f3ecfb250c7a5c1d6a2
Gavin Li <gli@iparadigms.com>
2016-10-20 19:22:54 -0700

    404

---
+++ Tree/Easy/404.Sum_of_Left_Leaves.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def sumOfLeftLeaves(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root is None:
+            return 0
+            
+        return self.helper(root.left, True) + self.helper(root.right, False)
+        
+    def helper(self, root, l):
+        if root is None:
+            return 0
+            
+        if root.left is None and root.right is None and l:
+            return root.val
+            
+        return self.helper(root.left, True) + self.helper(root.right, False)
+
+class Solution(object):
+    def sumOfLeftLeaves(self, root):
+        if not root: return 0
+        if root.left and not root.left.left and not root.left.right:
+            return root.left.val + self.sumOfLeftLeaves(root.right)
+        return self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)   # isn't leave
