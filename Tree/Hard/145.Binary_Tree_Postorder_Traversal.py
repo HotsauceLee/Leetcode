435e08c954b428b4c8b5faa9d6407c9a0c455f81
Gavin Li <yueqi.li.usa@gmail.com>
2016-10-29 22:08:56 -0700

    145 wiht last popped

--- Tree/Hard/145.Binary_Tree_Postorder_Traversal.py
+++ Tree/Hard/145.Binary_Tree_Postorder_Traversal.py
@@ -5,7 +5,7 @@
 #         self.left = None
 #         self.right = None
 
-#=================== Stack ===================
+#=================== Stack Destroy original tree===================
 # Time: O(n)
 # Space: O(log(n))
 # Running time: ~40ms
@@ -37,3 +37,67 @@ class Solution(object):
             
         return result
         
+
+#==================== Stack with last popped ==================
+# Time: O(n)
+# Space: O(log(n))
+# Running time: ~35ms
+class Solution(object):
+
+    def postorderTraversal(self, root):
+
+        """
+
+        :type root: TreeNode
+
+        :rtype: List[int]
+
+        """
+
+        result = []
+
+        if root is None:
+
+            return result
+
+            
+
+        stack = [root]
+
+        last_popped = root
+
+        while stack:
+
+            top_node = stack[-1]
+
+            # Push left
+
+                # top.left is not null && top.left is not last_popped && top.right is not last_popped
+
+            # Push right
+
+                # top.right is not null && top.right is not lst_popped && (top.left is null || top.left is last_popped)
+
+            # Otherwise Pop
+
+
+
+            if top_node.left is not None and top_node.left is not last_popped and top_node.right is not last_popped:
+
+                stack.append(top_node.left)
+
+            elif top_node.right is not None and top_node.right is not last_popped and (top_node.left is None or top_node.left is last_popped):
+
+                stack.append(top_node.right)
+
+            else:
+
+                last_node = stack.pop()
+
+                last_popped = last_node
+
+                result.append(last_node.val)
+
+                
+
+        return result
