9ec2f1ee4d92b8ad7040e026b2c71656f0d3c48e
Gavin Li <yueqi.li.usa@gmail.com>
2016-11-01 21:29:41 -0700

    129 bfs dfs

--- Tree/Medium/R__129.Sum_Root_to_Leaf_Numbers.py
+++ Tree/Medium/R__129.Sum_Root_to_Leaf_Numbers.py
@@ -26,3 +26,35 @@ class Solution(object):
             return plus_self
         
         return self.helper(root.left, plus_self) + self.helper(root.right, plus_self)
+
+# dfs + stack
+def sumNumbers1(self, root):
+    if not root:
+        return 0
+    stack, res = [(root, root.val)], 0
+    while stack:
+        node, value = stack.pop()
+        if node:
+            if not node.left and not node.right:
+                res += value
+            if node.right:
+                stack.append((node.right, value*10+node.right.val))
+            if node.left:
+                stack.append((node.left, value*10+node.left.val))
+    return res
+    
+# bfs + queue
+def sumNumbers2(self, root):
+    if not root:
+        return 0
+    queue, res = collections.deque([(root, root.val)]), 0
+    while queue:
+        node, value = queue.popleft()
+        if node:
+            if not node.left and not node.right:
+                res += value
+            if node.left:
+                queue.append((node.left, value*10+node.left.val))
+            if node.right:
+                queue.append((node.right, value*10+node.right.val))
+    return res
