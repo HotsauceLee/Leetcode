2ded8fac54f5a096893bf48e785888769504e968
Gavin Li <gli@iparadigms.com>
2016-11-16 16:40:46 -0800

    1

--- Easy/R__1.Two_Sum.py
+++ Easy/R__1.Two_Sum.py
@@ -2,24 +2,9 @@
 # Time: O(n)
 # Space: O(n)
 class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        dict = {}
-        for idx, val in enumerate(nums):
-            if dict.has_key(val):
-                if val + val == target:
-                    return [idx, dict[val]] if idx < dict[val] else [dict[val], idx]
-                else:
-                    continue
-            
-            dict[val] = idx
-            if dict.has_key(target - val):
-                if idx == dict[target - val]:
-                    continue
-                return [idx, dict[target - val]] if idx < dict[target - val] else [dict[target - val], idx]
-                
-        return None
+	d = {}
+	for idx, val in enumerate(nums):
+		if d.has_key(target - val): return [d[target - val], idx]
+		d[val] = idx
+
+	return None
