609ca2bb4b30dcbe6e170cd0d1d4152f46b03186
Gavin Li <yueqi.li.usa@gmail.com>
2016-11-20 15:50:35 -0800

    285

--- Tree/Medium/R__285.Inorder_Successor_in_BST.py
+++ Tree/Medium/R__285.Inorder_Successor_in_BST.py
@@ -32,6 +32,11 @@ class Solution(object):
         return right if right else root
 
 #============= Pointer inorder Traversal ===========
+"""
+The inorder traversal of a BST is the nodes in ascending order. To find a successor, you just need to find the smallest one that is larger than the given value since there are no duplicate values in a BST. It just like the binary search in a sorted list. The time complexity should be O(h) where h is the depth of the result node. succ is a pointer that keeps the possible successor. Whenever you go left the current root is the new possible successor, otherwise the it remains the same.
+
+Only in a balanced BST O(h) = O(log n). In the worst case h can be as large as n.
+"""
 class Solution(object):
 
     def inorderSuccessor(self, root, p):
