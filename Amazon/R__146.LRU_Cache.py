34cd900769558ca65d786c49d6b0f95aa4caad5b
Gavin Li <yueqi.li.usa@gmail.com>
2016-12-01 21:31:19 -0800

    146 finally!

--- Amazon/R__146.LRU_Cache.py
+++ Amazon/R__146.LRU_Cache.py
@@ -1,3 +1,6 @@
+#================ deque ===================
+# Time: Get: O(n), Set: O(n)
+# Space: O(n)
 from collections import deque
 
 class LRUCache1(object):
@@ -53,6 +56,9 @@ class DLL(object):
         self.prev = None
         self.next = None
 
+# ============== Double Linked List ===================
+# Time: Get = Set = O(1)
+# Space: O(n)
 class LRUCache2(object):
 
     def __init__(self, capacity):
@@ -78,16 +84,8 @@ class LRUCache2(object):
         if self.d.has_key(key):
             node = self.d[key]
             self.move_to_head(key)
-            print "get({}) return: {}".format(key, node.val)
-            print "volumn: {}, cap: {}".format(self.vol, self.cap)
-            for i in self.d:
-                print "{}, {}".format(i, self.d[i].val) 
             return node.val
             
-        print "get({}) return: {}".format(key, -1)
-        print "volumn: {}, cap: {}".format(self.vol, self.cap)
-        for i in self.d:
-            print "{}, {}".format(i, self.d[i].val) 
         return -1
         
 
@@ -97,55 +95,49 @@ class LRUCache2(object):
         :type value: int
         :rtype: nothing
         """
-        print "set({}, {})".format(key, value)
         if self.cap <= 0: reutrn
+
         if self.d.has_key(key):
             node = self.d[key]
             node.val = value
             self.move_to_head(key)
-            print "volumn: {}, cap: {}".format(self.vol, self.cap)
-            for i in self.d:
-                print "{}, {}".format(i, self.d[i].val)
             return
         
         if self.vol >= self.cap:
             self.pop()
         
-        new_node = self.append(key, value)
+        self.add_new(key, value)
+        
+    def add_new(self, key, value):
+        new_node = DLL(key, value)
         self.d[key] = new_node
-        print "volumn: {}, cap: {}".format(self.vol, self.cap)
-        for i in self.d:
-            print "{}, {}".format(i, self.d[i].val)
-        
-        
+        self.append(key)
+        self.vol += 1
+
     def move_to_head(self, key):
-        value_node = self.d[key]
         self.remove(key)
-        self.append(key, value_node.val)
+        self.append(key)
         
     def remove(self, key):
         node = self.d[key]
         node.prev.next = node.next
         node.next.prev = node.prev
-        self.vol -= 1
         
-    def append(self, key, value):
-        node = DLL(key, value)
+    def append(self, key):
+        node = self.d[key]
         node.prev = self.head
         node.next = self.head.next
-        node.next.prev = node
+        self.head.next.prev = node
         self.head.next = node
-        self.vol += 1
-        
-        return node
         
     def pop(self):
-        del self.d[self.tail.prev.key]
-        self.tail = self.tail.prev
-        self.tail.next = None
-
+        key = self.tail.prev.key
+        self.remove(key)
+        del self.d[key]
         self.vol -= 1
         
+
+
 def test():
     print "================= Test Case 0 =================="
     l = LRUCache2(2)
@@ -166,9 +158,6 @@ def test1():
     l2.get(1)
     l2.get(2)
 
-"""
-1,[set(2,1),get(2),set(3,2),get(2),get(3)]
-"""
 def test2():
     print "================= Test Case 2 =================="
     l = LRUCache2(1)
@@ -289,11 +278,6 @@ def test3():
     l.set(9,26)
     l.set(13,28)
     l.set(11,26)
-
-#test()
-#test1()
-#test2()
-test3()
 
 text = """
 set(10,13),set(3,17),set(6,11),set(10,5),set(9,10),get(13),set(2,19),get(2),get(3),set(5,25),get(8),set(9,22),set(5,5),set(1,30),get(11),set(9,12),get(7),get(5),get(8),get(9),set(4,30),set(9,3),get(9),get(10),get(10),set(6,14),set(3,1),get(3),set(10,11),get(8),set(2,14),get(1),get(5),get(4),set(11,4),set(12,24),set(5,18),get(13),set(7,23),get(8),get(12),set(3,27),set(2,12),get(5),set(2,9),set(13,4),set(8,18),set(1,7),get(6),set(9,29),set(8,21),get(5),set(6,30),set(1,12),get(10),set(4,15),set(7,22),set(11,26),set(8,17),set(9,29),get(5),set(3,4),set(11,30),get(12),set(4,29),get(3),get(9),get(6),set(3,4),get(1),get(10),set(3,29),set(10,28),set(1,20),set(11,13),get(3),set(3,12),set(3,8),set(10,9),set(3,26),get(8),get(7),get(5),set(13,17),set(2,27),set(11,15),get(12),set(9,19),set(2,15),set(3,16),get(1),set(12,17),set(9,1),set(6,19),get(4),get(5),get(5),set(8,1),set(11,7),set(5,2),set(9,28),get(1),set(2,2),set(7,4),set(4,22),set(7,24),set(9,26),set(13,28),set(11,26)
