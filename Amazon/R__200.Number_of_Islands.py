7b69ab42b695a3f2ceca27f68bad1a3bff1b8fe2
Gavin Li <yueqi.li.usa@gmail.com>
2016-12-03 08:35:37 -0800

    200

--- Amazon/R__200.Number_of_Islands.py
+++ Amazon/R__200.Number_of_Islands.py
@@ -7,19 +7,15 @@ class Solution(object):
         row = len(grid)
         if row == 0: return 0
         column = len(grid[0])
-        if column == 0: return 0
-        d = {}
         result = 0
         
         def bfs(r, c):
             if r >= row or r < 0 or c >= column or c < 0:
                 return
             if grid[r][c] == '0':
-                return  
-            if d.has_key((r, c)):
                 return
             
-            d[(r, c)] = 1
+            grid[r][c] = '0'
             bfs(r + 1, c)
             bfs(r - 1, c)
             bfs(r, c + 1)
@@ -28,7 +24,7 @@ class Solution(object):
             
         for r in range(row):
             for c in range(column):
-                if grid[r][c] == '0' or d.has_key((r, c)):
+                if grid[r][c] == '0':
                     continue
                 bfs(r, c)
                 result += 1
