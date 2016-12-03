0fb62154d9dcf6ad5a9137d2c2e14695cb36dbae
Gavin Li <yueqi.li.usa@gmail.com>
2016-12-03 08:12:46 -0800

    200

---
+++ Amazon/R__200.Number_of_Islands.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        row = len(grid)
+        if row == 0: return 0
+        column = len(grid[0])
+        if column == 0: return 0
+        d = {}
+        result = 0
+        
+        def bfs(r, c):
+            if r >= row or c >= column:
+                return
+            if grid[r][c] == '0':
+                return
+            if d.has_key((r, c)):
+                return
+            
+            d[(r, c)] = 1
+            bfs(r + 1, c)
+            bfs(r - 1, c)
+            bfs(r, c + 1)
+            bfs(r, c - 1)
+            
+            
+        for r in range(row):
+            for c in range(column):
+                if grid[r][c] == '0' or d.has_key((r, c)):
+                    continue
+                bfs(r, c)
+                result += 1
+                
+        return result
