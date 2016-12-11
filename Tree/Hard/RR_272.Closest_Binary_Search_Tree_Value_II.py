3c1ee2a38fd6cfa2ffe7731986a529e1df75413c
Gavin Li <yueqi.li.usa@gmail.com>
2016-12-11 14:28:11 -0800

    272

--- Tree/Hard/RR_272.Closest_Binary_Search_Tree_Value_II.py
+++ Tree/Hard/RR_272.Closest_Binary_Search_Tree_Value_II.py
@@ -35,3 +35,77 @@ class Solution(object):
         s.append(root.val)
         
         self.inorder(root.left if reverse else root.right, reverse, target, s)
+        
+# ============== getPredecessor + getSuccessor ===================
+# Time: O((2+k) * log(n) --> klog(n))
+# Space: O(log(n))
+class Solution(object):
+    def closestKValues(self, root, target, k):
+        """
+        :type root: TreeNode
+        :type target: float
+        :type k: int
+        :rtype: List[int]
+        """
+        # log(n)
+        self.pred_stack = self.initPredStack(root, target)
+        # log(n)
+        self.succ_stack = self.initSuccStack(root, target)
+        if self.pred_stack and self.succ_stack and self.pred_stack[-1].val == self.succ_stack[-1].val:
+            self.getSuccessor()
+            
+        result = []
+        # k
+        while k > 0:
+            # log(n) each
+            if not self.pred_stack: result.append(self.getSuccessor().val)
+            elif not self.succ_stack: result.append(self.getPredecessor().val)
+            elif abs(float(self.pred_stack[-1].val) - target) < abs(float(self.succ_stack[-1].val) - target):
+                result.append(self.getPredecessor().val)
+            else:
+                result.append(self.getSuccessor().val)
+            k -= 1
+            
+        return result
+        
+    def initPredStack(self, root, target):
+        pred_stack = []
+        while root:
+            if target >= root.val:
+                pred_stack.append(root)
+                root = root.right
+            else:
+                root = root.left
+        return pred_stack
+        
+    def initSuccStack(self, root, target):
+        succ_stack = []
+        while root:
+            if target <= root.val:
+                succ_stack.append(root)
+                root = root.left
+            else:
+                root = root.right
+        return succ_stack
+        
+    def getPredecessor(self):
+        if self.pred_stack:
+            pred = self.pred_stack.pop()
+            pred_left = pred.left
+            while pred_left:
+                self.pred_stack.append(pred_left)
+                pred_left = pred_left.right
+            return pred
+            
+        return None
+        
+    def getSuccessor(self):
+        if self.succ_stack:
+            succ = self.succ_stack.pop()
+            succ_right = succ.right
+            while succ_right:
+                self.succ_stack.append(succ_right)
+                succ_right = succ_right.left
+            return succ
+            
+        return None
