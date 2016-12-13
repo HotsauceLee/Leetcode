956e1e64766d0ee2a154426f28625dad8192034f
Gavin Li <gli@iparadigms.com>
2016-12-12 18:09:26 -0800

    169

---
+++ Array/Easy/R__169.Majority_Element.py
@@ -0,0 +1,40 @@
+#============== Dict ===============
+# Time: O(n)
+# Space: O(n)
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        majority = nums[0]
+        d = {nums[0]: 1}
+        for i in nums[1:]:
+            if not d.has_key(i):
+                d[i] = 1
+            else:
+                d[i] += 1
+                if d[i] > d[majority]:
+                    majority = i
+                        
+        return majority
+#============ Boyer–Moore majority vote algorithm ==========
+# Time: O(n)
+# Space: O(1)
+# NOTE: majority must exist!
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        majority = nums[0]
+        count = 1
+        for i in nums[1:]:
+            if not count:
+                majority = i
+                count = 1
+            elif i == majority: count += 1
+            else:count -= 1
+                        
+        return majority
