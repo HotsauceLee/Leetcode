2b0ebea2a8950342eeae31d25ab6919aeb118ecc
Gavin Li <yueqi.li.usa@gmail.com>
2016-12-12 21:30:33 -0800

    145

--- Tree/Hard/R__145.Binary_Tree_Postorder_Traversal.py
+++ Tree/Hard/R__145.Binary_Tree_Postorder_Traversal.py
@@ -113,4 +113,45 @@ class Solution(object):
 # ================== Morris Traversal ===================
 # Time: O(n)
 # Space: (1)
-
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        result = []
+        dump = TreeNode(-1)
+        dump.left = root
+        while dump:
+            if not dump.left:
+                dump = dump.right
+            else:
+                predecessor = dump.left
+                while predecessor.right and predecessor.right is not dump:
+                    predecessor = predecessor.right
+                if not predecessor.right:
+                    predecessor.right = dump
+                    dump = dump.left
+                else:
+                    self.store_reverse(dump.left, predecessor, result)
+                    dump = dump.right
+                    
+        return result
+        
+    def reverse(self, frm, to):
+        if frm is to: return
+        nxt = frm.right
+        tail = None
+        while frm is not to:
+            tail = nxt.right
+            nxt.right = frm
+            frm = nxt
+            nxt = tail
+            
+    def store_reverse(self, frm, to, result):
+        self.reverse(frm, to)
+        while True:
+            result.append(to.val)
+            if to is frm: break
+            to = to.right
+        self.reverse(to, frm)
