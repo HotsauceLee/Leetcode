62e7039fb393258a51b4753664429ab976c6e33a
Gavin Li <yueqi.li.usa@gmail.com>
2016-12-15 07:52:07 -0800

    414

---
+++ Array/Easy/R__414.Third_Maximum_Number.py
@@ -0,0 +1,28 @@
+#============ 3 pointers ===============
+# Time: O(3n)
+# Space: O(1)
+class Solution(object):
+    def thirdMax(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        first = second = third = float('-inf')
+        for n in nums:
+            if n in [first, second, third]:
+                continue
+            
+            if n > first:
+                third = second
+                second = first
+                first = n
+            elif n > second:
+                third = second
+                second = n
+            elif n > third:
+                third = n
+                
+        return first if third == float('-inf') else third
+        
+# ============= Priority Queue ===================
+# TODO:
