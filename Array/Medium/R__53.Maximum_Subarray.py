23b8cdf9481a2a05994a460593e16703ba06e945
Gavin Li <yueqi.li.usa@gmail.com>
2016-12-20 17:48:45 -0800

    53

--- Array/Medium/R__53.Maximum_Subarray.py
+++ Array/Medium/R__53.Maximum_Subarray.py
@@ -13,3 +13,21 @@ class Solution(object):
             max_sum = max(max_so_far, max_sum)
             
         return max_sum
+# ================== DP ===================
+# Timee: O(n)
+# Space: O(n)
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dp = [0]*len(nums)
+        dp[0] = nums[0]
+        max_sub = dp[0]
+        
+        for i in xrange(1, len(nums)):
+            dp[i] = nums[i] + dp[i - 1] if dp[i - 1] > 0 else nums[i]
+            max_sub = max(max_sub, dp[i])
+            
+        return max_sub
