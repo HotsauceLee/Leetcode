f0e9e3bbc6498062a4c4d83d74086fcebc700339
Gavin Li <yueqi.li.usa@gmail.com>
2016-12-22 19:31:32 -0800

    53

--- Array/Medium/R__53.Maximum_Subarray.py
+++ Array/Medium/R__53.Maximum_Subarray.py
@@ -1,6 +1,19 @@
 #================ Kadane's algorithm =============
 # Time: O(n)
 # Space: O(1)
+# Idea:
+#    algorithm that operates on arrays: it starts at
+#    the left end (element A[1]) and scans through to
+#    the right end (element A[n]), keeping track of 
+#    the maximum sum subvector seen so far. The maximum
+#    is initially A[0]. Suppose we've solved the problem
+#    for A[1 .. i - 1]; how can we extend that to A[1 .. i]?
+#    The maximum sum in the first I elements is either 
+#    the maximum sum in the first i - 1 elements (which 
+#    we'll call MaxSoFar), or it is that of a subvector 
+#    that ends in position i (which we'll call MaxEndingHere).
+#    MaxEndingHere is either A[i] plus the previous MaxEndingHere,
+#    or just A[i], whichever is larger.
 class Solution(object):
     def maxSubArray(self, nums):
         """
