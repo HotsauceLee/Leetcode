a99fca664e7f7bb4edfe09db89787499a4b69c66
Gavin Li <yueqi.li.usa@gmail.com>
2016-12-26 14:21:44 -0800

    106

--- Tree/Medium/R__106.Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.py
+++ Tree/Medium/R__106.Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.py
@@ -27,3 +27,27 @@ class Solution(object):
         root.left = self.buildTree(inorder[:inorderIndex], postorder)
 
         return root
+
+#=============== Same but use pointers ================
+# Time: O(n)
+# Space: O(n) - won't copy lists every time
+class Solution(object):
+    def buildTree(self, inorder, postorder):
+        """
+        :type inorder: List[int]
+        :type postorder: List[int]
+        :rtype: TreeNode
+        """
+        def helper(i_begin, i_end):
+            if i_begin > i_end or p_end[0] < 0: return None
+            
+            root = TreeNode(postorder[p_end[0]])
+            p_end[0] -= 1
+            root_index = inorder.index(root.val)
+            root.right = helper(root_index + 1, i_end)
+            root.left = helper(i_begin, root_index - 1)
+            
+            return root
+        
+        p_end = [len(postorder) - 1]
+        return helper(0, len(inorder) - 1)
