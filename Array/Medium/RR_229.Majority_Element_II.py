f30cdd0971a125a9961e1bcf84916f04931e7502
Gavin Li <yueqi.li.usa@gmail.com>
2016-12-27 21:01:47 -0800

    229

---
+++ Array/Medium/RR_229.Majority_Element_II.py
@@ -0,0 +1,57 @@
+# ======= Dict ==========
+# Time: O(n)
+# Space: O(n)
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        d = {}
+        second = first = float('-inf')
+        for n in nums:
+            if d.has_key(n):
+                d[n] += 1
+            else:
+                d[n] = 1
+            
+            if d[n] > len(nums)/3:
+                if n in [first, second]:
+                    continue
+                elif d[n] > first:
+                    second = first
+                    first = n
+                elif d[n] > second:
+                    second = n
+        
+        result = []
+        if first > float('-inf'):
+            result.append(first)
+        if second > float('-inf'):
+            result.append(second)
+            
+        return result
+        
+# ========== Boyer-Moore =================
+# Time: O(n + n + n)
+# Space: O(1)
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        candidate1, candidate2, count1, count2 = 0.1, 0.1, 0, 0
+        for n in nums:
+            if candidate1 == n:
+                count1 += 1
+            elif candidate2 == n:
+                count2 += 1
+            elif count1 == 0:
+                candidate1, count1 = n, 1
+            elif count2 == 0:
+                candidate2, count2 = n, 1
+            else:
+                count1, count2 = count1 - 1, count2 - 1
+        
+        return [n for n in [candidate1, candidate2] if nums.count(n) > len(nums)//3]
