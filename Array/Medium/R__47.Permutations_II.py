100cd68b7a47f157f05fe4fdf8de076ae2ffc30b
Gavin Li <yueqi.li.usa@gmail.com>
2016-12-29 21:41:54 -0800

    47

--- Array/Medium/R__47.Permutations_II.py
+++ Array/Medium/R__47.Permutations_II.py
@@ -25,3 +25,32 @@ class Solution(object):
         result = []
         helper(nums, [], result)
         return result
+    
+#=========== sort + back tracking less space and time ===========
+# Time: ?
+# Space: ?
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        def helper(nums, cur_path, result, used):
+            if len(cur_path) == len(nums):
+                result.append(cur_path[:])
+                return
+            
+            for i in xrange(len(nums)):
+                if used[i] or (i > 0 and nums[i] == nums[i - 1] and not used[i - 1]):
+                    continue
+                cur_path.append(nums[i])
+                used[i] = True
+                helper(nums, cur_path, result, used)
+                used[i] = False
+                cur_path.pop()
+        
+        nums.sort()     
+        result = []
+        used = [False]*len(nums)
+        helper(nums, [], result, used)
+        return result
