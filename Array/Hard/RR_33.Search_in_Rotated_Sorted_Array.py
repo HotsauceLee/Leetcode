8022f424488e92474403998c75c36a6f1c3ded7e
Gavin Li <yueqi.li.usa@gmail.com>
2017-01-04 21:47:50 -0800

    33

--- Array/Hard/RR_33.Search_in_Rotated_Sorted_Array.py
+++ Array/Hard/RR_33.Search_in_Rotated_Sorted_Array.py
@@ -1,5 +1,11 @@
-# http://fisherlei.blogspot.com/2013/01/leetcode-search-in-rotated-sorted-array.html
-
+# ============= Binary search ================
+# Time: O(log(n))
+# Space: O(1)
+# Idea:
+"""
+[__larger__|_____smaller_____]
+If target in the larger zone, go left. otherwise, go right.
+"""
 class Solution(object):
     def search(self, nums, target):
         """
@@ -7,13 +13,27 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
+        """
+        Corner cases:
+        1. [0] 0
+        2. [0] 1
+        3. [1,2] 2
+        4. [2,1] 1
+        5. [4,5,6,7,0,1,2] 5
+        6. [4,5,6,7,0,1,2] 1
+        
+        """
         left, right = 0, len(nums) - 1
         while left <= right:
             mid = (left + right)//2
             if nums[mid] == target: return mid
             # Larger zone. Floor divison leans toward left, so add equals here.
+            # case [2,1] 1
             if nums[mid] >= nums[left]:
-                # Target in the larger zone?
+                # Target in the smaller zone? If target == nums[mid],
+                # we won't be here, so add equals to the left.
+                # Also make sense to think that we have to include the
+                # whole range inclusively
                 if nums[left] <= target < nums[mid]:
                     # Go to larger zone
                     right = mid - 1
@@ -21,9 +41,11 @@ class Solution(object):
                     # Go to right half
                     left = mid + 1
             # Smaller zone.
+            # Case [5,1,3] 3
             else:
-                # Target in the smaller zone? If target == nums[mid],
-                # we won't be here, so add equals to the right.
+                # Target in the larger zone?
+                # Also make sense to think that we have to include the
+                # whole range inclusively, so add equals to the left end.
                 if nums[mid] < target <= nums[right]:
                     # Go to smaller zone
                     left = mid + 1
