c95276adb38ddf80b7ebc51843aad233a6e34493
Gavin Li <yueqi.li.usa@gmail.com>
2017-01-07 13:53:25 -0800

    189

--- Array/Easy/R__189.Rotate_Array.py
+++ Array/Easy/R__189.Rotate_Array.py
@@ -32,6 +32,13 @@ class Solution(object):
 # ============== Reverse =================
 # Time: O(n): n/2 + k/2 + (n-k)/2 = n
 # Space: O(1)
+"""
+        n=7, k=3
+        [1,2,3,4,5,6,7]
+        [7,6,5,4,3,2,1]
+        [5,6,7,...]
+        [...1,2,3,4]
+        """
 class Solution(object):
     def rotate(self, nums, k):
         """
