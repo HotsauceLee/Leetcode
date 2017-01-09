02e3d2441de65b8ccda95282119f7f92b4721aaf
Gavin Li <yueqi.li.usa@gmail.com>
2017-01-08 19:38:33 -0800

    414

--- Array/Easy/R__414.Third_Maximum_Number.py
+++ Array/Easy/R__414.Third_Maximum_Number.py
@@ -23,6 +23,19 @@ class Solution(object):
                 third = n
                 
         return first if third == float('-inf') else third
-        
-# ============= Priority Queue ===================
-# TODO:
+
+# ============= Python set + maxium ==============
+# Time: O(4n)
+# Space: O(1)
+class Solution(object):
+    def thirdMax(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums = set(nums)
+        if len(nums) < 3:
+            return max(nums)
+        nums.remove(max(nums))
+        nums.remove(max(nums))
+        return max(nums)
