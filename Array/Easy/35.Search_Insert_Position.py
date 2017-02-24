a4a854084e46a5da086fa91bed41b8d1f1d6668d
Gavin Li <yueqi.li.usa@gmail.com>
2017-02-24 14:42:18 -0800

    Update 35.Search_Insert_Position.py

--- Array/Easy/35.Search_Insert_Position.py
+++ Array/Easy/35.Search_Insert_Position.py
@@ -33,3 +33,24 @@ class Solution(object):
             return end + 1 
 
         return end 
+    
+# ============= Cleaner version ==============
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left, right = 0, len(nums)
+        while left < right:
+            mid = (left + right)/2
+            if nums[mid] == target:
+                return mid
+                
+            if nums[mid] > target:
+                right = mid
+            else:
+                left = mid + 1
+                
+        return left
