aff7c8c7c192010e0a6c500730d128e46128fc3b
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-01 14:50:57 -0800

    Update and rename 287.Find_the_Duplicate_Number.py to RR_287.Find_the_Duplicate_Number.py

--- Array/Medium/RR_287.Find_the_Duplicate_Number.py
+++ Array/Medium/RR_287.Find_the_Duplicate_Number.py
@@ -1,4 +1,4 @@
-# =========== Use array value as index =============
+# =========== Use array value as index(changed array value, not allowed!) =============
 # Time: O(n)
 # Space: O(1)
 class Solution(object):
@@ -15,3 +15,7 @@ class Solution(object):
             if nums[idx] < 0:
                 return abs(nums[i])
             nums[idx] = -nums[idx]
+
+# =========== Binary search ==============:
+# Time: O(nlog(n))
+# Space: O(1)
