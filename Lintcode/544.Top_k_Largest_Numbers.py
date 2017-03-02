4e8cbed43b05542889a0dd8877f7a1703e835a44
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-01 20:10:29 -0800

    Create 544.Top_k_Largest_Numbers.py

---
+++ Lintcode/544.Top_k_Largest_Numbers.py
@@ -0,0 +1,20 @@
+# ============ Heap =============
+# Time: O(nlog(n))
+# Space: O(n)
+import heapq
+class Solution:
+    '''
+    @param {int[]} nums an integer array
+    @param {int} k an integer
+    @return {int[]} the top k largest numbers in array
+    '''
+    def topk(self, nums, k):
+        # Write your code here
+        heap = []
+        for n in nums:
+            heapq.heappush(heap, n)
+            if len(heap) > k:
+                heapq.heappop(heap)
+        
+        heap.sort(reverse=True)
+        return heap
