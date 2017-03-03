50a012c0babb10daddba7c6bae960cb06fd48e11
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-02 16:47:51 -0800

    Update RR_287.Find_the_Duplicate_Number.py

--- Array/Medium/RR_287.Find_the_Duplicate_Number.py
+++ Array/Medium/RR_287.Find_the_Duplicate_Number.py
@@ -37,3 +37,23 @@ class Solution(object):
 # =========== Find the entry point of the loop ============
 # Time: O(n)
 # Space: O(1)
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums or len(nums) <= 1:
+            return 0
+            
+        slow, fast = nums[0], nums[nums[0]]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            
+        fast = 0
+        while fast != slow:
+            slow = nums[slow]
+            fast = nums[fast]
+            
+        return slow
