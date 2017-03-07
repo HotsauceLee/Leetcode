a0484078e18289fcb354c4a4aa9f7d55254755b2
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-07 07:58:19 -0800

    Update R__4.Ugly_Number_II.py

--- Lintcode/R__4.Ugly_Number_II.py
+++ Lintcode/R__4.Ugly_Number_II.py
@@ -1,3 +1,16 @@
+"""
+Ugly number is a number that only have factors 2, 3 and 5.
+
+Design an algorithm to find the nth ugly number. The first 10 ugly numbers are 1, 2, 3, 4, 5, 6, 8, 9, 10, 12...
+
+ Notice
+
+Note that 1 is typically treated as an ugly number.
+
+Have you met this question in a real interview? Yes
+Example
+If n=9, return 10.
+"""
 # ============= Heap keep adding newly generated uglies ===========
 # Time: O(n3log(n))
 # Space: O(3n^2)
