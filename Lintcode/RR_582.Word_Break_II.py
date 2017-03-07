2ec99fd1ada7b0d5a80f40b98bd76b70d7633233
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-07 07:55:56 -0800

    Update RR_582.Word_Break_II.py

--- Lintcode/RR_582.Word_Break_II.py
+++ Lintcode/RR_582.Word_Break_II.py
@@ -1,3 +1,15 @@
+"""
+Given a string s and a dictionary of words dict, add spaces in s to construct a sentence where each word is a valid dictionary word.
+
+Return all such possible sentences.
+
+Have you met this question in a real interview? Yes
+Example
+Gieve s = lintcode,
+dict = ["de", "ding", "co", "code", "lint"].
+
+A solution is ["lint code", "lint co de"].
+"""
 # ============ Backtracking + DP ===============
 # Time:
 # Space:
