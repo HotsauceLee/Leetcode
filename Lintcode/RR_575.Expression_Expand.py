8dfca4ca1fd98bb9cb52d6d74e5b8234f609f784
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-07 07:55:12 -0800

    Update RR_575.Expression_Expand.py

--- Lintcode/RR_575.Expression_Expand.py
+++ Lintcode/RR_575.Expression_Expand.py
@@ -1,3 +1,13 @@
+"""
+Given an expression s includes numbers, letters and brackets. Number represents the number of repetitions inside the brackets(can be a string or another expression)．Please expand expression to be a string.
+
+Have you met this question in a real interview? Yes
+Example
+s = abc3[a] return abcaaa
+s = 3[abc] return abcabcabc
+s = 4[ac]dy, return acacacacdy
+s = 3[2[ad]3[pf]]xyz, return adadpfpfpfadadpfpfpfadadpfpfpfxyz
+"""
 # =========== Stack ============
 # Time: O(n)
 # Space: O(n)
