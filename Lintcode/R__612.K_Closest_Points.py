992e7667059439e3033d1e2d11b0e444ad885935
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-07 07:59:42 -0800

    Update R__612.K_Closest_Points.py

--- Lintcode/R__612.K_Closest_Points.py
+++ Lintcode/R__612.K_Closest_Points.py
@@ -1,3 +1,12 @@
+"""
+Given some points and a point origin in two dimensional space, find k points out of the some points which are nearest to origin.
+Return these points sorted by distance, if they are same with distance, sorted by x-axis, otherwise sorted by y-axis.
+
+Have you met this question in a real interview? Yes
+Example
+Given points = [[4,6],[4,7],[4,4],[2,5],[1,1]], origin = [0, 0], k = 3
+return [[1,1],[2,5],[4,4]]
+"""
 # ============= Heap + Custom Comparator =============
 # Time: O(nlog(n))
 # Space: O(n)
