44f3706863d79a911f0ee473d8a88c0d8cefc666
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-07 07:57:42 -0800

    Update R__31.Partition_Array.py

--- Lintcode/R__31.Partition_Array.py
+++ Lintcode/R__31.Partition_Array.py
@@ -1,3 +1,20 @@
+"""
+Given an array nums of integers and an int k, partition the array (i.e move the elements in "nums") such that:
+
+All elements < k are moved to the left
+All elements >= k are moved to the right
+Return the partitioning index, i.e the first index i nums[i] >= k.
+
+ Notice
+
+You should do really partition in array nums instead of just counting the numbers of integers smaller than k.
+
+If all elements in nums are smaller than k, then return nums.length
+
+Have you met this question in a real interview? Yes
+Example
+If nums = [3,2,2,1] and k=2, a valid answer is 1.
+"""
 # ============ Left and right pointers =============
 # Time: O(n)
 # Space: O(1)
