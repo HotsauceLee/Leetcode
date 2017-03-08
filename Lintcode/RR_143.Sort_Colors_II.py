401b1443db429037fc4876792d659be7fb658061
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-07 16:32:43 -0800

    Update RR_143.Sort_Colors_II.py

--- Lintcode/RR_143.Sort_Colors_II.py
+++ Lintcode/RR_143.Sort_Colors_II.py
@@ -1,3 +1,16 @@
+"""
+Given an array of n objects with k different colors (numbered from 1 to k), sort them so that objects of the same color are adjacent, with the colors in the order 1, 2, ... k.
+
+ Notice
+
+You are not suppose to use the library's sort function for this problem.
+
+k <= n
+
+Have you met this question in a real interview? Yes
+Example
+Given colors=[3, 2, 2, 1, 4], k=4, your code should sort colors in-place to [1, 2, 2, 3, 4].
+"""
 # ============== binary Search divide to two groups at a time ==============
 # Time: O(nlog(n))
 # Space: O(1)
