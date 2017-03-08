721fb15c1dcdb65602d2a2de042615a495547d87
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-07 16:27:18 -0800

    Update 606.Kth_Largest_Element_II.py

--- Lintcode/606.Kth_Largest_Element_II.py
+++ Lintcode/606.Kth_Largest_Element_II.py
@@ -1,3 +1,16 @@
+"""
+Find K-th largest element in an array. and N is much larger than k.
+
+ Notice
+
+You can swap elements in the array
+
+Have you met this question in a real interview? Yes
+Example
+In array [9,3,2,4,8], the 3rd largest element is 4.
+
+In array [1,2,3,4,5], the 1st largest element is 5, 2nd largest element is 4, 3rd largest element is 3 and etc.
+"""
 # ================ Heap ==================
 # Time: O(nlog(n))
 # Space: O(n)
