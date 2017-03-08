6f80bbf56a030683283bcb985dec9a240e72520d
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-07 16:40:07 -0800

    Update R__587.Two_Sum-Unique_pairs.py

--- Lintcode/R__587.Two_Sum-Unique_pairs.py
+++ Lintcode/R__587.Two_Sum-Unique_pairs.py
@@ -1,3 +1,14 @@
+"""
+Given an array of integers, find how many unique pairs in the array such that their sum is equal to a specific target number. Please return the number of pairs.
+
+Have you met this question in a real interview? Yes
+Example
+Given nums = [1,1,2,45,46,46], target = 47
+return 2
+
+1 + 46 = 47
+2 + 45 = 47
+"""
 # ============ Left and right pointers ==============
 # Time: O(n)
 # Space: O(1)
