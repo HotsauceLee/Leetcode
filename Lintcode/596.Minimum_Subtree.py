825a8b0fd80b79604255b057f9f699968925e14a
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-07 16:25:37 -0800

    Update 596.Minimum_Subtree.py

--- Lintcode/596.Minimum_Subtree.py
+++ Lintcode/596.Minimum_Subtree.py
@@ -1,3 +1,22 @@
+"""
+Given a binary tree, find the subtree with minimum sum. Return the root of the subtree.
+
+ Notice
+
+LintCode will print the subtree which root is your return node.
+It's guaranteed that there is only one subtree with minimum sum and the given binary tree is not an empty tree.
+
+Have you met this question in a real interview? Yes
+Example
+Given a binary tree:
+
+     1
+   /   \
+ -5     2
+ / \   /  \
+0   2 -4  -5 
+return the node 1.
+"""
 # ============= Recursion =============
 # Time: O(n)
 # Space: O(n)
