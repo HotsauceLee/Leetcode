8a7a8d58afe545a9b2a07454156b2ed05ee90c0d
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-07 16:18:05 -0800

    Update 111.Climbing_Stairs.py

--- Lintcode/111.Climbing_Stairs.py
+++ Lintcode/111.Climbing_Stairs.py
@@ -1,3 +1,14 @@
+"""
+You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Have you met this question in a real interview? Yes
+Example
+Given an example n=3 , 1+1+1=2+1=1+2=3
+
+return 3
+"""
 # ============= DP variables =============
 # Time: O(n)
 # Space: O(1)
