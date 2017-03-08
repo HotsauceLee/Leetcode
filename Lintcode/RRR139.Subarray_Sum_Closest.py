333e7e1513a9467c533587ad3cf99d93bb8332fa
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-07 16:30:08 -0800

    Update RRR139.Subarray_Sum_Closest.py

--- Lintcode/RRR139.Subarray_Sum_Closest.py
+++ Lintcode/RRR139.Subarray_Sum_Closest.py
@@ -1,3 +1,10 @@
+"""
+Given an integer array, find a subarray with sum closest to zero. Return the indexes of the first number and last number.
+
+Have you met this question in a real interview? Yes
+Example
+Given [-3, 1, 1, -3, 5], return [0, 2], [1, 3], [1, 1], [2, 2] or [0, 4].
+"""
 # ============= Custom Comparator =============
 # Time: O(nlog(n) + n)
 # Space: O(n)
