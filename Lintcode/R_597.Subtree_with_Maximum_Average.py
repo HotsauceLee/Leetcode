340a39bc279d0523d675895271878968d38c4be1
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-07 16:36:32 -0800

    Update R_597.Subtree_with_Maximum_Average.py

--- Lintcode/R_597.Subtree_with_Maximum_Average.py
+++ Lintcode/R_597.Subtree_with_Maximum_Average.py
@@ -1,3 +1,24 @@
+"""
+Given a binary tree, find the subtree with maximum average. Return the root of the subtree.
+
+ Notice
+
+LintCode will print the subtree which root is your return node.
+It's guaranteed that there is only one subtree with maximum average.
+
+Have you met this question in a real interview? Yes
+Example
+Given a binary tree:
+
+     1
+   /   \
+ -5     11
+ / \   /  \
+1   2 4    -2 
+return the node 11.
+
+
+"""
 # ============= Recursion ==============
 # Time: O(n)
 # Space: O(n)
