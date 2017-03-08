bd6aa84154a2b5a7530292ce8c6f0dc0e7c104cc
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-07 16:35:59 -0800

    Update R_578.Lowest_Common_Ancestor_III.py

--- Lintcode/R_578.Lowest_Common_Ancestor_III.py
+++ Lintcode/R_578.Lowest_Common_Ancestor_III.py
@@ -1,3 +1,27 @@
+"""
+Given the root and two nodes in a Binary Tree. Find the lowest common ancestor(LCA) of the two nodes.
+The lowest common ancestor is the node with largest depth which is the ancestor of both nodes.
+Return null if LCA does not exist.
+
+ Notice
+
+node A or node B may not exist in tree.
+
+Have you met this question in a real interview? Yes
+Example
+For the following binary tree:
+
+  4
+ / \
+3   7
+   / \
+  5   6
+LCA(3, 5) = 4
+
+LCA(5, 6) = 7
+
+LCA(6, 7) = 7
+"""
 # ============== Recursion ================
 # Time: O(n)
 # Space: O(n)
