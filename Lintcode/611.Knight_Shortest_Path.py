c9e00ed8e713b82d35b1fd5f7caadf5b5c93162f
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-07 16:27:52 -0800

    Update 611.Knight_Shortest_Path.py

--- Lintcode/611.Knight_Shortest_Path.py
+++ Lintcode/611.Knight_Shortest_Path.py
@@ -1,3 +1,40 @@
+"""
+Given a knight in a chessboard (a binary matrix with 0 as empty and 1 as barrier) with a source position, find the shortest path to a destination position, return the length of the route. 
+Return -1 if knight can not reached.
+
+ Notice
+
+source and destination must be empty.
+Knight can not enter the barrier.
+
+Have you met this question in a real interview? Yes
+Clarification
+If the knight is at (x, y), he can get to the following positions in one step:
+
+(x + 1, y + 2)
+(x + 1, y - 2)
+(x - 1, y + 2)
+(x - 1, y - 2)
+(x + 2, y + 1)
+(x + 2, y - 1)
+(x - 2, y + 1)
+(x - 2, y - 1)
+Example
+[[0,0,0],
+ [0,0,0],
+ [0,0,0]]
+source = [2, 0] destination = [2, 2] return 2
+
+[[0,1,0],
+ [0,0,0],
+ [0,0,0]]
+source = [2, 0] destination = [2, 2] return 6
+
+[[0,1,0],
+ [0,0,1],
+ [0,0,0]]
+source = [2, 0] destination = [2, 2] return -1
+"""
 # ============ BFS =============
 # Time: O(2n)
 # Space: O(n)
