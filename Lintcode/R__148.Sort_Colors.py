5e2ba469a59b7df87d09598ef57054af8651b050
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-07 16:38:25 -0800

    Update R__148.Sort_Colors.py

--- Lintcode/R__148.Sort_Colors.py
+++ Lintcode/R__148.Sort_Colors.py
@@ -1,3 +1,17 @@
+"""
+Given an array with n objects colored red, white or blue, sort them so that objects of the same color are adjacent, with the colors in the order red, white and blue.
+
+Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+
+ Notice
+
+You are not suppose to use the library's sort function for this problem. 
+You should do it in-place (sort numbers in the original array).
+
+Have you met this question in a real interview? Yes
+Example
+Given [1, 0, 1, 2], sort it in-place to [0, 1, 1, 2].
+"""
 # ========== 3 pointers grouping ========
 # Time: O(n)
 # Space: O(1)
