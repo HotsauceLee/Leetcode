39f17bf33bfd3e165342f85cd6cb806f48ff293a
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-07 16:18:39 -0800

    Update 115.Unique_Paths_II.py

--- Lintcode/115.Unique_Paths_II.py
+++ Lintcode/115.Unique_Paths_II.py
@@ -1,3 +1,26 @@
+"""
+Follow up for "Unique Paths":
+
+Now consider if some obstacles are added to the grids. How many unique paths would there be?
+
+An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+
+ Notice
+
+m and n will be at most 100.
+
+Have you met this question in a real interview? Yes
+Example
+For example,
+There is one obstacle in the middle of a 3x3 grid as illustrated below.
+
+[
+  [0,0,0],
+  [0,1,0],
+  [0,0,0]
+]
+The total number of unique paths is 2.
+"""
 # ========== DP ===========
 # Time: O(m*n)
 # Space: O(1)
