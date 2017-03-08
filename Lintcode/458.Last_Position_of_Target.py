b4d3b4d34685704ba87942349d51a47cc0fcc78a
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-07 16:21:45 -0800

    Update 458.Last_Position_of_Target.py

--- Lintcode/458.Last_Position_of_Target.py
+++ Lintcode/458.Last_Position_of_Target.py
@@ -1,3 +1,16 @@
+"""
+Find the last position of a target number in a sorted array. Return -1 if target does not exist.
+
+Have you met this question in a real interview? Yes
+Example
+Given [1, 2, 2, 4, 5, 5].
+
+For target = 2, return 2.
+
+For target = 5, return 5.
+
+For target = 6, return -1.
+"""
 # ================ Binary Search ================
 # Time: O(log(n))
 # Space: O(1)
