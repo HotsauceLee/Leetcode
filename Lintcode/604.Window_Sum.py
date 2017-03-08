5b0c290e3c40a3c08d30ddca4a350d569bc03e2c
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-07 16:26:48 -0800

    Update 604.Window_Sum.py

--- Lintcode/604.Window_Sum.py
+++ Lintcode/604.Window_Sum.py
@@ -1,3 +1,14 @@
+"""
+Given an array of n integer, and a moving window(size k), move the window at each iteration from the start of the array, find the sum of the element inside the window at each moving.
+
+Have you met this question in a real interview? Yes
+Example
+For array [1,2,7,8,5], moving window size k = 3. 
+1 + 2 + 7 = 10
+2 + 7 + 8 = 17
+7 + 8 + 5 = 20
+return [10,17,20]
+"""
 # =========== Add next substract last ===========
 # Time: O(n)
 # Space: O(1)
