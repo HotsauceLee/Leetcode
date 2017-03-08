396f2cdda6b6ec4880645fa3e8758d9bdca97ebb
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-07 16:40:43 -0800

    Update R__599.Insert_into_a_Cyclic_Sorted_List.py

--- Lintcode/R__599.Insert_into_a_Cyclic_Sorted_List.py
+++ Lintcode/R__599.Insert_into_a_Cyclic_Sorted_List.py
@@ -1,3 +1,17 @@
+"""
+Given a node from a cyclic linked list which has been sorted, write a function to insert a value into the list such that it remains a cyclic sorted list. The given node can be any single node in the list. Return the inserted new node.
+
+ Notice
+
+3->5->1 is a cyclic list, so 3 is next node of 1.
+3->5->1 is same with 5->1->3
+
+Have you met this question in a real interview? Yes
+Example
+Given a list, and insert a value 4:
+3->5->1
+Return 5->1->3->4
+"""
 # ==========================
 # Time: O(n)
 # Space: O(1)
