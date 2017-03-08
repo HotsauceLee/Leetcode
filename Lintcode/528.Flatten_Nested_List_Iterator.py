e18cbe6e3e233198ab7dff177704a3409bdd3377
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-07 16:22:29 -0800

    Update 528.Flatten_Nested_List_Iterator.py

--- Lintcode/528.Flatten_Nested_List_Iterator.py
+++ Lintcode/528.Flatten_Nested_List_Iterator.py
@@ -1,3 +1,18 @@
+"""
+Given a nested list of integers, implement an iterator to flatten it.
+
+Each element is either an integer, or a list -- whose elements may also be integers or other lists.
+
+ Notice
+
+You don't need to implement the remove method.
+
+Have you met this question in a real interview? Yes
+Example
+Given the list [[1,1],2,[1,1]], By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,1,2,1,1].
+
+Given the list [1,[4,[6]]], By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,4,6].
+"""
 # ============ Stack ============
 # Time: init: O(N = all ints) next: O(1) hasNext: O(1)
 # Space: O(N = all ints)
