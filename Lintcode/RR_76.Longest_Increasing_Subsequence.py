4d0563fee8e1d2b4fcc1f518e317099e8786f4be
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-07 16:34:39 -0800

    Update RR_76.Longest_Increasing_Subsequence.py

--- Lintcode/RR_76.Longest_Increasing_Subsequence.py
+++ Lintcode/RR_76.Longest_Increasing_Subsequence.py
@@ -1,3 +1,20 @@
+"""
+Given a sequence of integers, find the longest increasing subsequence (LIS).
+
+You code should return the length of the LIS.
+
+Have you met this question in a real interview? Yes
+Clarification
+What's the definition of longest increasing subsequence?
+
+The longest increasing subsequence problem is to find a subsequence of a given sequence in which the subsequence's elements are in sorted order, lowest to highest, and in which the subsequence is as long as possible. This subsequence is not necessarily contiguous, or unique.
+
+https://en.wikipedia.org/wiki/Longest_increasing_subsequence
+
+Example
+For [5, 4, 1, 2, 3], the LIS is [1, 2, 3], return 3
+For [4, 2, 4, 5, 3, 7], the LIS is [2, 4, 5, 7], return 4
+"""
 # ============= DP ===============
 # Time: O(n^2)
 # Space: O(n)
