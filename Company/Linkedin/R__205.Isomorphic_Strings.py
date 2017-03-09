ea7592e6defefca14db2d60e88048efb706cb6c4
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-08 20:02:48 -0800

    Update R__205.Isomorphic_Strings.py

--- Company/Linkedin/R__205.Isomorphic_Strings.py
+++ Company/Linkedin/R__205.Isomorphic_Strings.py
@@ -25,3 +25,29 @@ class Solution(object):
                     
         return True
 
+# ======== Record last appeared position ==========
+# Time: O(n)
+# Space: O(1)
+# Idea:
+"""
+If s[i] appeared before, check if t[i] appeared in the same position. If ture, update their positions.
+If s[i] never appeared before, then check if t[i] appeared before.
+"""
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if not len(s) and not len(t):
+            return True
+            
+        d1, d2 = [0]*256, [0]*256
+        for i in xrange(len(s)):
+            if d1[ord(s[i])] != d2[ord(t[i])]:
+                return False
+            d1[ord(s[i])] = i + 1
+            d2[ord(t[i])] = i + 1
+            
+        return True
