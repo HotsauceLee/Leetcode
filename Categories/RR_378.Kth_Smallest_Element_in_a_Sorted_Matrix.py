46f8368c7bfd8b30b946486e19cf638513dce9ff
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-12 13:52:45 -0700

    Create RR_378.Kth_Smallest_Element_in_a_Sorted_Matrix.py

---
+++ Categories/RR_378.Kth_Smallest_Element_in_a_Sorted_Matrix.py
@@ -0,0 +1,36 @@
+# =========== Heap ==============
+# Time: O(k2log(row))
+# Space: O(row)
+# Category: Kth smallest/biggest with heap
+class Node(object):
+    def __init__(self, val, row, col):
+        self.val = val
+        self.row = row
+        self.col = col
+        
+    def __cmp__(self, other):
+        return self.val - other.val
+
+import heapq
+class Solution(object):
+    def kthSmallest(self, matrix, k):
+        """
+        :type matrix: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        if not matrix or not matrix[0]:
+            return None
+        
+        len_row, len_col = len(matrix), len(matrix[0])
+        heap = [Node(matrix[row][0], row, 0) for row in xrange(len_row)]
+        result = None
+        while k >= 1:
+            cur_smallest = heapq.heappop(heap)
+            result = cur_smallest.val
+            k -= 1
+            if cur_smallest.col + 1 < len_col:
+                heapq.heappush(heap, Node(matrix[cur_smallest.row][cur_smallest.col + 1], cur_smallest.row, cur_smallest.col + 1))
+
+        return result
+        
