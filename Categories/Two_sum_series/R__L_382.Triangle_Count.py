de35da6265981b7b5474f3d5a729bb0faddf800c
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-13 20:01:08 -0700

    Update R__L_382.Triangle_Count.py

--- Categories/Two_sum_series/R__L_382.Triangle_Count.py
+++ Categories/Two_sum_series/R__L_382.Triangle_Count.py
@@ -1,3 +1,21 @@
+"""
+Given an array of integers, how many three numbers can be found in the array, so that we can build an triangle whose three edges length is the three numbers that we find?
+
+Have you met this question in a real interview? Yes
+Example
+Given array S = [3,4,6,7], return 3. They are:
+
+[3,4,6]
+[3,6,7]
+[4,6,7]
+Given array S = [4,4,4,4], return 4. They are:
+
+[4(1),4(2),4(3)]
+[4(1),4(2),4(4)]
+[4(1),4(3),4(4)]
+[4(2),4(3),4(4)]
+"""
+
 # ========= hi&lo pointers ==============
 # Time: O(n^2)
 # Space: O(1)
