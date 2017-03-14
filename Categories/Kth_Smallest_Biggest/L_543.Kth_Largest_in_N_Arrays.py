e820f57801f5684e0a9420516738898f5bf17377
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-13 19:29:54 -0700

    Create L_543.Kth_Largest_in_N_Arrays.py

---
+++ Categories/Kth_Smallest_Biggest/L_543.Kth_Largest_in_N_Arrays.py
@@ -0,0 +1,19 @@
+# =========== heap ===========
+# Time: O(nlog(k))
+# Space: O(k)
+import heapq
+class Solution:
+    # @param {int[][]} arrays a list of array
+    # @param {int} k an integer
+    # @return {int} an integer, K-th largest element in N arrays
+    def KthInArrays(self, arrays, k):
+        # Write your code here
+        heap = []
+        for a in arrays:
+            for n in a:
+                heapq.heappush(heap, n)
+                if len(heap) > k:
+                    heapq.heappop(heap)
+                    
+        
+        return heapq.heappop(heap)
