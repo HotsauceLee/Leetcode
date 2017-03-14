5adcb5d77f8a35c992a53020877c9de721be2d03
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-13 20:00:28 -0700

    Update RR_L_465.Kth_Smallest_Sum_In_Two_Sorted_Arrays.py

--- Categories/Kth_Smallest_Biggest/RR_L_465.Kth_Smallest_Sum_In_Two_Sorted_Arrays.py
+++ Categories/Kth_Smallest_Biggest/RR_L_465.Kth_Smallest_Sum_In_Two_Sorted_Arrays.py
@@ -1,3 +1,17 @@
+"""
+Given two integer arrays sorted in ascending order and an integer k. Define sum = a + b, where a is an element from the first array and b is an element from the second one. Find the kth smallest sum out of all possible sums.
+
+Have you met this question in a real interview? Yes
+Example
+Given [1, 7, 11] and [2, 4, 6].
+
+For k = 3, return 7.
+
+For k = 4, return 9.
+
+For k = 8, return 15.
+"""
+
 # =========== heap + hashset ===========
 # Time: O(k*3*log(k^2)) = O(klog(k))
 # Space: heap: O(n) set: O(k)
