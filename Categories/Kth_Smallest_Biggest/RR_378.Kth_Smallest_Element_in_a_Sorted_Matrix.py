7089c61698807e60862eec2338d4ccb84ea82fe5
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-13 20:00:01 -0700

    Update RR_378.Kth_Smallest_Element_in_a_Sorted_Matrix.py

--- Categories/Kth_Smallest_Biggest/RR_378.Kth_Smallest_Element_in_a_Sorted_Matrix.py
+++ Categories/Kth_Smallest_Biggest/RR_378.Kth_Smallest_Element_in_a_Sorted_Matrix.py
@@ -1,3 +1,18 @@
+"""
+Find the kth smallest number in at row and column sorted matrix.
+
+Have you met this question in a real interview? Yes
+Example
+Given k = 4 and a matrix:
+
+[
+  [1 ,5 ,7],
+  [3 ,7 ,8],
+  [4 ,8 ,9],
+]
+return 5
+"""
+
 # =========== Heap ==============
 # Time: O(k2log(row))
 # Space: O(row)
