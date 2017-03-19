53b2bfcd1df416991390feb02e75659c709c9171
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-18 19:34:04 -0700

    Update Union_Find.py

--- Templates/Union_Find.py
+++ Templates/Union_Find.py
@@ -1,1 +1,20 @@
-
+# Trap: when doing union, change father2's father to father1, NOT node2's father to father1!!!!
+class UnionFind(object):
+    def __init__(self, nodes):
+        self.father = {}
+        for n in nodes:
+            self.father[n] = n
+        
+    def find(self, node):
+        if self.father[node] == node:
+            return node
+            
+        f = self.find(self.father[node])
+        self.father[node] = f
+        return f
+        
+    def union(self, node1, node2):
+        f1 = self.find(node1)
+        f2 = self.find(node2)
+        if f1 != f2:
+            self.father[f2] = f1
