b2bcb010d3bc31d6b63e14d3d3465f53fbd3112e
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-20 20:34:12 -0700

    Update Union_Find.py

--- Templates/Union_Find.py
+++ Templates/Union_Find.py
@@ -19,6 +19,36 @@ class UnionFind(object):
         f2 = self.find(node2)
         if f1 != f2:
             self.father[f2] = f1
+            
+            
+# =========== Non-recursion find =================
+class UnionFind(object):
+    def __init__(self, nodes):
+        self.father = {}
+        for n in nodes:
+            self.father[n] = n
+        
+    def find(self, node):
+        if self.father[node] == node:
+            return node
+        
+        f = node
+        f = self.father[f]
+        while f != self.father[f]:
+            f = self.father[f]
+
+        compress = node
+        while compress != self.father[compress]:
+            compress = self.father[compress]
+            self.father[compress] = f
+
+        return f
+        
+    def union(self, node1, node2):
+        f1 = self.find(node1)
+        f2 = self.find(node2)
+        if f1 != f2:
+            self.father[f2] = f1
 
 # ================ Keep the number of subsets ===================
 class UnionFind(object):
