a8f782812e0dc3bad08838235591f1e2639087db
Gavin Li <yueqi.li.usa@gmail.com>
2017-03-20 19:54:49 -0700

    Update Union_Find.py

--- Templates/Union_Find.py
+++ Templates/Union_Find.py
@@ -1,3 +1,4 @@
+# ================ Only checks whether two elements are in the same set ===================
 # Trap: when doing union, change father2's father to father1, NOT node2's father to father1!!!!
 class UnionFind(object):
     def __init__(self, nodes):
@@ -18,3 +19,58 @@ class UnionFind(object):
         f2 = self.find(node2)
         if f1 != f2:
             self.father[f2] = f1
+
+# ================ Keep the number of subsets ===================
+class UnionFind(object):
+    def __init__(self, nodes):
+        self.father = {}
+        self.count = 0
+        for n in nodes:
+            self.father[n] = n
+            self.count += 1
+        
+    def find(self, node):
+        if self.father[node] == node:
+            return node
+            
+        f = self.find(self.father[node])
+        self.father[node] = f
+        return f
+        
+    def union(self, node1, node2):
+        f1 = self.find(node1)
+        f2 = self.find(node2)
+        if f1 != f2:
+            self.father[f2] = f1
+            self.count -= 1
+            
+    def subset_count(self):
+        return self.count
+    
+# =============== Keeps the elements of each subset =============
+class UnionFind(object):
+    def __init__(self, nodes):
+        self.father = {}
+        self.fathers = {}
+        for n in nodes:
+            self.father[n] = n
+            self.fathers[n] = [n]
+
+    def find(self, node):
+        if self.father[node] == node:
+            return node
+            
+        f = self.find(self.father[node])
+        self.father[node] = f
+        return f
+        
+    def union(self, node1, node2):
+        f1 = self.find(node1)
+        f2 = self.find(node2)
+        if f1 != f2:
+            self.father[f1] = f2
+            self.fathers[f2] += self.fathers[f1]
+            del self.fathers[f1]
+            
+    def subsets(self):
+        return self.fathers
