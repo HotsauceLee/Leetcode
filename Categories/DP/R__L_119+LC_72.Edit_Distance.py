d3e4daf0dfe960d2a14e0fbeb4a0bc496fae309c
Gavin Li <yueqi.li.usa@gmail.com>
2017-05-06 15:37:46 -0700

    Create R__L_119+LC_72.Edit_Distance.py

---
+++ Categories/DP/R__L_119+LC_72.Edit_Distance.py
@@ -0,0 +1,41 @@
+"""
+Given two words word1 and word2, find the minimum number of steps required to convert word1 to word2. (each operation is counted as 1 step.)
+
+You have the following 3 operations permitted on a word:
+
+Insert a character
+Delete a character
+Replace a character
+Have you met this question in a real interview? Yes
+Example
+Given word1 = "mart" and word2 = "karma", return 3.
+"""
+
+# ================ DP =================
+# Time: O(len(word1) + len(word1)*len(word2))
+# Space: O(len(word1))
+class Solution(object):
+    def minDistance(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        if not word1 and not word2:
+            return 0
+        if not word1 or not word2:
+            return len(word1) or len(word2)
+            
+        prev_row = [i for i in xrange(len(word1) + 1)]
+            
+        for row in xrange(1, len(word2) + 1):
+            cur_row = [row]
+            for col in xrange(1, len(word1) + 1):
+                if word1[col - 1] == word2[row - 1]:
+                    cur_row.append(prev_row[col - 1])
+                else:
+                    cur_row.append(min(min(cur_row[col - 1], prev_row[col]), prev_row[col - 1]) + 1)
+            prev_row = cur_row
+                    
+        return cur_row[-1]
+        
