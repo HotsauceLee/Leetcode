b460cf654f84c63a3b8e22aeea737f8ff95d86a8
Gavin Li <yueqi.li.usa@gmail.com>
2017-06-03 14:26:25 -0700

    Create R__451.Sort_Characters_By_Frequency.py

---
+++ Categories/Sorting/R__451.Sort_Characters_By_Frequency.py
@@ -0,0 +1,76 @@
+"""
+Given a string, sort it in decreasing order based on the frequency of characters.
+
+Example 1:
+
+Input:
+"tree"
+
+Output:
+"eert"
+
+Explanation:
+'e' appears twice while 'r' and 't' both appear once.
+So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+Example 2:
+
+Input:
+"cccaaa"
+
+Output:
+"cccaaa"
+
+Explanation:
+Both 'c' and 'a' appear three times, so "aaaccc" is also a valid answer.
+Note that "cacaca" is incorrect, as the same characters must be together.
+Example 3:
+
+Input:
+"Aabb"
+
+Output:
+"bbAa"
+
+Explanation:
+"bbaA" is also a valid answer, but "Aabb" is incorrect.
+Note that 'A' and 'a' are treated as two different characters.
+"""
+
+# ================ store every char/one char in result =============
+# time: O(n + nlog(n) + n)
+# Space: O(n)
+class Node(object):
+    def __init__(self, ch, count, first_index):
+        self.ch = ch
+        self.count = count
+        self.i = first_index
+
+    def __cmp__(self, other):
+        if self.count != other.count:
+            return other.count - self.count
+        return self.i - other.i
+        
+    def __str__(self):
+        return self.ch*self.count # delete *self.count if only want one char in result
+
+class Solution(object):
+    def frequencySort(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if not s:
+            return ""
+            
+        d = {}
+        for idx, c in enumerate(s):
+            if c in d:
+                d[c].count += 1
+            else:
+                d[c] = Node(c, 1, idx)
+                
+        char_list = sorted(d.values())
+        result = ''
+        for n in char_list:
+            result += str(n)
+        return result
